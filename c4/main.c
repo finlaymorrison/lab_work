4e6d353b3a88a46b8ae29a1166d92d1922727ce2
Finlay Morrison <morrison.fin02@gmail.com>
2020-10-28 12:45:48 +0000

    empty

--- c4/main.c
+++ c4/main.c
@@ -8,13 +8,45 @@
 void create_letter_histogram(const char* filename, int* histogram);
 void print_array(int const* array, int size);
 void print_histogram(int const* histogram, int size, int terminal_width);
+void encipher(const char *p, char *c, const unsigned int offset);
+void decipher(const char *c, char *p, const unsigned int offset);
 
 int main(int argc, char** argv)
 {
 	int histogram[ALPHABET_CHARARECTER_COUNT];
 	create_letter_histogram("bee_movie_script.txt", histogram);
-	print_histogram(histogram, ALPHABET_CHARARECTER_COUNT, TERMINAL_WIDTH);
+	char pp[10];
+	char aa[10];
+	const char* ppp = "dhjeiqldu";
+	encipher(ppp, pp, 1);
+	decipher(pp, aa, 1);
+	printf("%s\n", aa);
+	//print_histogram(histogram, ALPHABET_CHARARECTER_COUNT, TERMINAL_WIDTH);
 	return 0;
+}
+
+void encipher(const char *p, char *c, const unsigned int offset)
+{
+	char buffer;
+	int index = 0;
+	while(buffer = p[index])
+	{
+		c[index] = buffer + offset;
+		++index;
+	}
+	c[index] = '\0';
+}
+
+void decipher(const char *c, char *p, const unsigned int offset)
+{
+	char buffer;
+	int index = 0;
+	while(buffer = c[index])
+	{
+		p[index] = buffer - offset;
+		++index;
+	}
+	p[index] = '\0';
 }
 
 void create_letter_histogram(const char* filename, int* histogram)
@@ -53,7 +85,7 @@ void print_histogram(int const* histogram, int size, int terminal_width)
 			max_value = histogram[i];
 		}
 	}
-	float scale = max_value / terminal_width;
+	float scale = (float)max_value / terminal_width;
 	for (int i = 0; i < size; ++i)
 	{
 		int bar_size = histogram[i] / scale;
