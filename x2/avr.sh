967879502cbe0285e044ee6bc7e3222d686f274c
Finlay Morrison <morrison.fin02gmail.com>
2020-11-02 23:10:22 +0000

    x2

--- x2/avr.sh
+++ x2/avr.sh
@@ -1,5 +1,3 @@
 avr-gcc -mmcu=atmega644p -DF_CPU=12000000 -Wall -Os "$@" -o a.elf
 avr-objcopy -O ihex a.elf a.hex
-rm a.elf
-avrdude -c usbasp -p m644p -U flash:w:a.hex
-rm a.hex
+avrdude -c c232hm -p m644p -U flash:w:a.hex
